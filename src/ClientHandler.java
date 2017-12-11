54b891d169a6bee29dacaeb98dddd1c3bb76059c
Luc Hayward <luchay10@gmail.com>
2017-12-11 19:17:13 +0200

    Add Client handler

---
+++ src/ClientHandler.java
@@ -0,0 +1,14 @@
+public class CientHandler implements Runnable{
+	Socket connectionSock;
+
+	public CientHandler(){
+		connectionSock = null;
+	}
+
+	public CientHandler(Socket connectionSock){
+		this.connectionSock = connectionSock;
+	}
+
+	p
+	
+}
