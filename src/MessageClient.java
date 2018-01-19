6c9c0ac358dce7aec44e999164c2788087dc4ef5
Luc Hayward <luchay10@gmail.com>
2018-01-19 19:04:28 +0200

    Seperated handling of client connections from the connections themselves. Cleaned misc. code

--- src/MessageClient.java
+++ src/MessageClient.java
@@ -8,54 +8,14 @@ import java.util.Scanner;
 /**
  * Rudimentary Client class (See Absolute Java p1140)
  */
-public class Client {
+public class MessageClient {
 
-	private String hostName;
-	private int port;
+	private static String hostName;
+	private static int port = 8976;
 
-	Client(String hostName, int port) {
-		this.hostName = hostName;
-		this.port = port;
-
-		initialise();
-	}
-
-
-	/**
-	 * Initialise the client instance.
-	 */
-	private void initialise() {
-		try {
-			System.out.println("Connecting to server on port: " + port);
-
-			//Creates a stream socket and connects it to the specified port number on the named host.
-			Socket connectionSock = new Socket(hostName, port);
-
-			BufferedReader serverInput = new BufferedReader(new InputStreamReader(connectionSock.getInputStream()));
-			DataOutputStream serverOutput = new DataOutputStream(connectionSock.getOutputStream());
-
-			System.out.println("Connection established, sending test string.");
-
-			serverOutput.writeBytes("Test");
-			System.out.println("Waiting on reply.");
-
-			String serverResponse = serverInput.readLine();
-			System.out.println("Received from server: " + serverResponse);
-
-			serverInput.close();
-			serverOutput.close();
-			connectionSock.close();
-
-		}
-		catch (IOException e) {
-			System.err.println(e.getMessage());
-		}
-
-	}
 
 	public static void main(String[] args) {
-		int port = 8976;
-		String hostName;
+
 
 		Scanner inKb = new Scanner(new InputStreamReader(System.in));
 		String line = "";
--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,64 +9,24 @@ import java.net.Socket;
 public class Server {
 
 
-	private int portNum;
-
-	Server(int portNum) {
-		this.portNum = portNum;
-		initialise();
-
-	}
-
-	/**
-	 * Initialises the server
-	 */
-	private void initialise() {
-		try {
-			System.out.println("Initialising server on port " + portNum + ".");
-
-			ServerSocket serverSock = new ServerSocket(portNum);
-
-			//ServerSocket.accept() returns the socket connecting to this server, allows the client to connect proactively.
-			Socket connectionSock = serverSock.accept();
-
-			// Use a BufferedReader as it reads the data stream rather than blocking until
-			// receiving an end of line character.
-			BufferedReader clientInput = new BufferedReader(new InputStreamReader(connectionSock.getInputStream()));
-			DataOutputStream clientOutput = new DataOutputStream(connectionSock.getOutputStream());
-
-			System.out.println("Waiting for client to respond.");
-
-			String clientMessage = clientInput.readLine();
-			System.out.println("Received from client: "+clientMessage);
-			clientOutput.writeBytes("Received: "+clientMessage);
-
-			clientOutput.close();
-			clientInput.close();
-			connectionSock.close();
-			serverSock.close();
-
-		}
-		catch (IOException e) {
-			System.err.println(e.getMessage());
-		}
-	}
-
+	private static int portNum = 8976;
+	private static ClientHandler handler;
 
 	public static void main(String[] args){
 		System.out.println("Running standalone Client on port 8976.");
-      
-      try {
-			System.out.println("Initialising server on port " + 8976 + ".");
-			ServerSocket serverSock = new ServerSocket(8976);
+		System.out.println("Running standalone Client on port 8976.");
+		handler = new ClientHandler();
+
+		try {
+			System.out.println("Initialising server on port " + portNum + ".");
+			ServerSocket serverSock = new ServerSocket(portNum);
 
 		  while (true) {
 			  //ServerSocket.accept() returns the socket connecting to this server, allows the client to connect proactively.
 			  Socket connectionSock = serverSock.accept();
 			  System.out.println("Connection received");
-			  ClientHandler handler = new ClientHandler(connectionSock);
-			  Thread newThread = new Thread(handler);
-			  newThread.start();
 
+			  handler.addClientConnection(connectionSock);
 		  }
 
 
